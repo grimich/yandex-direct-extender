454c36325e2465b38eff701f82dcbe65502babb3
grimich <grimich@users.noreply.github.com>
2015-05-11 21:08:54 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function checkForValidUrl(tabId, changeInfo, tab) {
-	if (tab.url.indexOf('linkedin.com') > -1) {
+	if (tab.url.indexOf('direct.yandex') > -1) {
 		chrome.pageAction.show(tabId);
 	}
 };
@@ -7,6 +7,5 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(tab.id, {file: "jquery-1.11.2.min.js"});
 	chrome.tabs.executeScript(tab.id, {file: "script.js"});
 });
