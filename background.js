662dd721cd4be14567c68397bf0e12ac6400d77c
grimich <github@grimich.ru>
2015-05-12 17:30:52 +0300

    Again

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
+/*function checkForValidUrl(tabId, changeInfo, tab) {
 	if (tab.url.indexOf('direct.yandex') > -1) {
 		chrome.pageAction.show(tabId);
 	}
@@ -9,3 +9,4 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
 chrome.pageAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(tab.id, {file: "script.js"});
 });
+*/
